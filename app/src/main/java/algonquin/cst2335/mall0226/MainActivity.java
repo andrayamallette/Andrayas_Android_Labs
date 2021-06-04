7793f12e2f1413071cd58aafc0d437717d57bdf3
Andraya Mallette <mall0226@algonquinlive.com>
2021-06-04 14:18:36 -0400

    Finished phone version of step 3

--- app/src/main/java/algonquin/cst2335/mall0226/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/mall0226/MainActivity.java
@@ -2,41 +2,36 @@ package algonquin.cst2335.mall0226;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.CheckBox;
-import android.widget.EditText;
-import android.widget.ImageButton;
+
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
-import android.widget.RadioButton;
 import android.widget.Switch;
-import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView imgView;
+    Switch sw;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        imgView= findViewById(R.id.flagview);
+        sw=findViewById(R.id.spin_switch);
 
-        TextView mytext = findViewById(R.id.textview);
-        Button mybutton = findViewById(R.id.mybutton);
-        CheckBox mycb = findViewById(R.id.mycb);
-        Switch mysc = findViewById(R.id.mysc);
-        RadioButton myradio = findViewById(R.id.myradio);
-        EditText myedit = findViewById(R.id.myedittext);
-
-        ImageView myimage = findViewById(R.id.logoalgonquin);
-        ImageButton imgbtn = findViewById( R.id.myimagebutton );
-
-        Context context = getApplicationContext();
-        mybutton.setOnClickListener( vw ->mytext.setText("your edit text has: " + myedit.getText().toString()));
-        mycb.setOnCheckedChangeListener((btn, isChecked)->Toast.makeText(context, "You clicked on the Checkbox and it is now: " + mycb.isChecked(), Toast.LENGTH_LONG).show());
-        mysc.setOnCheckedChangeListener((btn, isChecked)->Toast.makeText(context, "You clicked on the Switch and it is now: "+mysc.isChecked(), Toast.LENGTH_SHORT).show());
-        myradio.setOnCheckedChangeListener((btn, isChecked)->Toast.makeText(context, "You clicked on the Radio Button and it is now: " + myradio.isChecked(), Toast.LENGTH_SHORT).show());
-    //"The width = " + width + " and height = " + height.
-        imgbtn.setOnClickListener(vw-> Toast.makeText(context, "The width = " + imgbtn.getWidth() + " and height = " + imgbtn.getHeight(),Toast.LENGTH_SHORT).show());
+        sw.setOnCheckedChangeListener( (btn, isChecked) ->{
+            if (isChecked){
+                RotateAnimation rotate = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+                rotate.setDuration(5000);
+                rotate.setRepeatCount(Animation.INFINITE);
+                rotate.setInterpolator(new LinearInterpolator());
+            imgView.startAnimation(rotate);
+            }else{
+                imgView.clearAnimation();
+            }
+        });
     }
 }
