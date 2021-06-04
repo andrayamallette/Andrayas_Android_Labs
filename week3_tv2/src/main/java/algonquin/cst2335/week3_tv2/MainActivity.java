9f2dfef51a3e1b48ef0b98f1a4f36af61dad816d
Andraya Mallette <mall0226@algonquinlive.com>
2021-06-04 15:52:25 -0400

    Finished TV version of step 3

---
+++ week3_tv2/src/main/java/algonquin/cst2335/week3_tv2/MainActivity.java
@@ -0,0 +1,37 @@
+package algonquin.cst2335.week3_tv2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.Switch;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import android.widget.Switch;
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imgView;
+    Switch sw;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imgView= findViewById(R.id.flagview);
+        sw=findViewById(R.id.spin_switch);
+
+        sw.setOnCheckedChangeListener( (btn, isChecked) ->{
+            if (isChecked){
+                RotateAnimation rotate = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+                rotate.setDuration(5000);
+                rotate.setRepeatCount(Animation.INFINITE);
+                rotate.setInterpolator(new LinearInterpolator());
+                imgView.startAnimation(rotate);
+            }else{
+                imgView.clearAnimation();
+            }
+        });
+    }
+}
