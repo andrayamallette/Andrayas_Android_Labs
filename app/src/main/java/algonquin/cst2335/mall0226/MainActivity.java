3b2162e13aedcf5c0f4273b1ed37d6d25738c09c
Andraya Mallette <mall0226@algonquinlive.com>
2021-07-30 22:06:25 -0400

    Finished Week9 in XML it broke my vm

--- app/src/main/java/algonquin/cst2335/mall0226/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/mall0226/MainActivity.java
@@ -3,44 +3,38 @@ package algonquin.cst2335.mall0226;
 import androidx.appcompat.app.AppCompatActivity;
 
 
-import android.app.Activity;
-import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
 
 import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.lang.Character;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLEncoder;
-import java.nio.charset.StandardCharsets;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Executors;
-import java.util.stream.Collectors;
-
 /**
  * @author Andraya Mallette 040997475
  * @version 1.0
  */
 public class MainActivity extends AppCompatActivity {
 
+    String description = null;
+    String iconName = null;
+    String current = null;
+    String min = null;
+    String max = null;
+    String humidity = null;
 
     private String stringURL;
 
@@ -52,44 +46,58 @@ public class MainActivity extends AppCompatActivity {
 
         Button forecastBtn = findViewById(R.id.forecastButton);
         EditText cityText = findViewById(R.id.cityTextField);
+
+
         forecastBtn.setOnClickListener((click) ->{
+
+            String cityName=cityText.getText().toString();
+            AlertDialog dialog = new AlertDialog.Builder(MainActivity.this)
+                    .setTitle("Getting forecase")
+                    .setMessage("We're ca;;ing people in "+cityName + " to look outside their windows and tell us what's the weather like over there.")
+                    .setView(new ProgressBar(MainActivity.this))
+                    .show();
+
             Executor newThread = Executors.newSingleThreadExecutor();
             newThread.execute(()->{
                 try {
-
-                    String cityName = cityText.getText().toString();
                     stringURL ="https://api.openweathermap.org/data/2.5/weather?q="
                             + URLEncoder.encode(cityName, "UTF-8")
-                            + "&appid=7e943c97096a9784391a981c4d878b22&units=metric";
+                            + "&appid=7e943c97096a9784391a981c4d878b22&units=metric&mode=xml";
 
                     URL url = new URL(stringURL);
                     HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
                     InputStream in = new BufferedInputStream(urlConnection.getInputStream());
 
-                    String text = (new BufferedReader(
-                            new InputStreamReader(in, StandardCharsets.UTF_8)))
-                            .lines()
-                            .collect(Collectors.joining("\n"));
-
-                    JSONObject theDocument = new JSONObject(text);
-                    //not sure if these are needed but I'm adding anyways
-                    JSONObject coord = theDocument.getJSONObject("coord");
-                    JSONArray weatherArray = theDocument.getJSONArray("weather");
-                    JSONObject position0 = weatherArray.getJSONObject(0);
-                    String description = position0.getString("description");
-                    String iconName = position0.getString("icon");
-
-                    int vis = theDocument.getInt("visibility");
-                    String name = theDocument.getString("name");
-
-                    //meow
+                    XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+                    factory.setNamespaceAware(false);
+                    XmlPullParser xpp = factory.newPullParser();
+                    xpp.setInput(in, "UTF-8");
 
 
-                    JSONObject mainObject = theDocument.getJSONObject( "main" );
-                    double current = mainObject.getDouble("temp");
-                    double min = mainObject.getDouble("temp_min");
-                    double max = mainObject.getDouble( "temp_max");
-                    int humidity = mainObject.getInt("humidity");
+
+                    while( xpp.next() != XmlPullParser.END_DOCUMENT ){
+                        switch(xpp.getEventType()){
+                            case XmlPullParser.START_TAG:
+                                if(xpp.getName().equals("temperature")){
+                                    current = xpp.getAttributeValue(null, "value");  //this gets the current temperature
+
+                                    min = xpp.getAttributeValue(null, "min"); //this gets the min temperature
+
+                                    max = xpp.getAttributeValue(null, "max"); //this gets the max temperature
+                                }else if(xpp.getName().equals("weather")){
+                                    description = xpp.getAttributeValue(null, "value");  //this gets the weather description
+
+                                    iconName = xpp.getAttributeValue(null, "icon"); //this gets the icon name
+                                }else if(xpp.getName().equals("humidity")){
+                                    humidity=xpp.getAttributeValue(null, "value");
+                                }
+                                break;
+                            case XmlPullParser.END_TAG:
+                                break;
+                            case XmlPullParser.TEXT:
+                                break;
+                        }
+                    }
 
                     runOnUiThread(()->{
                         TextView tv = findViewById(R.id.temp);
@@ -111,37 +119,25 @@ public class MainActivity extends AppCompatActivity {
                         tv = findViewById(R.id.description);
                         tv.setText(description);
                         tv.setVisibility(View.VISIBLE);
+                        dialog.hide();
 
-                        ;
-                        ImageView iv = findViewById(R.id.icon);
+
+                        /*ImageView iv = findViewById(R.id.icon);
                         iv.setImageBitmap(assignImage(iconName));
-                        iv.setVisibility(View.VISIBLE);
+                        iv.setVisibility(View.VISIBLE);*/
                     });
 
 
 
-                } catch (IOException | JSONException ioe) {
+                } catch (IOException | XmlPullParserException ioe) {
                     ioe.printStackTrace();
                 }
             });
         });
 
-        /* deprecated code
-        tv = findViewById(R.id.editText);
-        et = findViewById(R.id.editText);
-        btn = findViewById(R.id.button);
-
-        btn.setOnClickListener(clk->{
-            String password=et.getText().toString();
-            if (checkPasswordComplexity(password)){
-                tv.setText("Your password meets the requirements");
-            }else if(!checkPasswordComplexity(password)){
-                tv.setText("You shall not pass!");
-            }
-        });
-        */
     }
 
+    /*
     private Bitmap assignImage(String iconName) {
         Bitmap image = null;
         try{
@@ -168,72 +164,6 @@ public class MainActivity extends AppCompatActivity {
         }
         return image;
     }
-
-    /**This function checks if the password has an upper and lower case letter a number and a special symbol
-     *
-     * @param password the String object that we are checking
-     * @return returns false if there lacks an upper/lower case letter, number, or a special character
      */
-    private boolean checkPasswordComplexity(String password) {
-        boolean foundUpperCase, foundLowerCase, foundNumber, foundSpecial;
-        foundUpperCase = foundLowerCase = foundNumber = foundSpecial = false;
 
-        Context context=getApplicationContext();
-        int duration = Toast.LENGTH_LONG;
-        for( int i=0; i<password.length(); i++){
-            char c = password.charAt(i);
-            if(Character.isUpperCase(c)){
-                foundUpperCase=true;
-            }else if(Character.isLowerCase(c)){
-                foundLowerCase=true;
-            }else if(Character.isDigit(c)){
-                foundNumber=true;
-            }else if(isSpecialCharacter(c)){
-                foundSpecial=true;
-            }
-        }
-        if(!foundUpperCase){
-            CharSequence text="You are missing an upper case";
-            Toast toast= Toast.makeText(context, text, duration);
-            toast.show();
-            return false;
-        }else if (!foundLowerCase){
-            CharSequence text="You are missing a lower case";
-            Toast toast= Toast.makeText(context, text, duration);
-            toast.show();
-            return false;
-        }else if (!foundNumber){
-            CharSequence text="You are missing a number";
-            Toast toast= Toast.makeText(context, text, duration);
-            toast.show();
-            return false;
-        }else if (!foundSpecial){
-            CharSequence text="You are missing a special character";
-            Toast toast= Toast.makeText(context, text, duration);
-            toast.show();
-            return false;
-        }else {
-            return true;
-        }
-
-    }
-
-    /**This method is used to determine if the parameter is one of these special characters "!@#$%^&*"
-     *
-     * @param c the char passed in that we are checking
-     * @return returns false if no special character is detected
-     */
-    private boolean isSpecialCharacter(char c){
-        switch (c){
-            case '!':
-            case '@':
-            case '#':
-            case '$':
-            case '%':
-            case '^':
-            case '&':
-            case '*': return true;
-            default: return false;
-        }
-    }
 }
